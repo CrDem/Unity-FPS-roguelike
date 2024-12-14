a753cc2b829db761705573e9f1806e7d3866edc1
Ibukhvalov <iabukhvalov@edu.hse.ru>
2024-12-14 23:28:17 +0300

    added jump to hero

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
     public float speed = 12f;
+    public float gravity = -9.81f * 2f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+
+    public bool isGrounded;
+    private bool isMoving;
+    
+    
     public float sprintSpeed = 18f; // Add a sprint speed
 
     void Start()
@@ -13,8 +25,14 @@ public class PlayerMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
+    void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+    
     void Update()
     {
+        CheckGround();
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -25,6 +43,8 @@ public class PlayerMovement : MonoBehaviour
         move.y = 0; // игнорируем вертикальную составляющую движения
         move.Normalize(); // Нормализуем вектор, чтобы его длина всегда была равна 1
 
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+        
         // Проверяем, если нажата клавиша Shift для бега
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -34,6 +54,14 @@ public class PlayerMovement : MonoBehaviour
         {
             controller.Move(move * speed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
     
     void OnControllerColliderHit(ControllerColliderHit hit)
